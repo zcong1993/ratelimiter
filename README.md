549c2bb31d3c9d76f360a82d138600a886231892
zcong1993 <zhangcong1992@gmail.com>
2020-01-10 17:59:44 +0800

    feat: add cpu stat

---
+++ README.md
@@ -0,0 +1,9 @@
+# ratelimiter
+
+[![NPM version](https://img.shields.io/npm/v/@zcong/ratelimiter.svg?style=flat)](https://npmjs.com/package/@zcong/ratelimiter) [![NPM downloads](https://img.shields.io/npm/dm/@zcong/ratelimiter.svg?style=flat)](https://npmjs.com/package/@zcong/ratelimiter) [![CircleCI](https://circleci.com/gh/zcong1993/ratelimiter/tree/master.svg?style=shield)](https://circleci.com/gh/zcong1993/ratelimiter/tree/master) [![codecov](https://codecov.io/gh/zcong1993/ratelimiter/branch/master/graph/badge.svg)](https://codecov.io/gh/zcong1993/ratelimiter)
+
+> my cool project
+
+## License
+
+MIT &copy; zcong1993
---
+++ src/cpu.ts
@@ -0,0 +1,97 @@
+import * as os from 'os'
+
+const getCpus = () => os.cpus()
+
+const getAllBusy = (t: os.CpuInfo): [number, number] => {
+  const idle: number = t.times.idle
+  const busy: number = t.times.irq + t.times.nice + t.times.sys + t.times.user
+
+  return [busy + idle, busy]
+}
+
+const calculateBusy = (t1: os.CpuInfo, t2: os.CpuInfo): number => {
+  const [t1All, t1Busy] = getAllBusy(t1)
+  const [t2All, t2Busy] = getAllBusy(t2)
+
+  if (t2Busy <= t1Busy) {
+    return 0
+  }
+
+  if (t2All <= t1All) {
+    return 100
+  }
+
+  return Math.min(100, Math.max(0, ((t2Busy - t1Busy) / (t2All - t1All)) * 100))
+}
+
+const calculateAllBusy = (t1: os.CpuInfo[], t2: os.CpuInfo[]): number[] => {
+  if (t1.length !== t2.length) {
+    throw new Error(`received two CPU counts: ${t1.length} != ${t2.length}`)
+  }
+
+  const res: number[] = []
+
+  for (let i = 0; i < t1.length; i += 1) {
+    res[i] = calculateBusy(t1[i], t2[i])
+  }
+
+  return res
+}
+
+const mergeAll = (t: os.CpuInfo[]): [os.CpuInfo] => {
+  const res: os.CpuInfo = {
+    model: '',
+    speed: 0,
+    times: {
+      idle: 0,
+      user: 0,
+      sys: 0,
+      irq: 0,
+      nice: 0
+    }
+  }
+
+  for (const tt of t) {
+    res.times.idle += tt.times.idle
+    res.times.user += tt.times.user
+    res.times.sys += tt.times.sys
+    res.times.irq += tt.times.irq
+    res.times.nice += tt.times.nice
+  }
+
+  return [res]
+}
+
+export class CpuStat {
+  private isFirstTime: boolean = true
+  private lastCpuInfo: os.CpuInfo[]
+  private cpuPercent: number = 0
+  private timer: NodeJS.Timeout
+
+  constructor(private readonly sampleInterval: number = 500) {
+    this.timer = setInterval(() => this.ticker(), this.sampleInterval)
+  }
+
+  ticker() {
+    if (this.isFirstTime) {
+      this.lastCpuInfo = getCpus()
+      this.isFirstTime = false
+      return
+    }
+
+    const nowInfo = getCpus()
+    const res = calculateAllBusy(mergeAll(this.lastCpuInfo), mergeAll(nowInfo))
+    this.cpuPercent = res[0]
+    this.lastCpuInfo = nowInfo
+  }
+
+  get stat() {
+    return this.cpuPercent
+  }
+
+  destroy() {
+    if (this.timer) {
+      clearInterval(this.timer)
+    }
+  }
+}
